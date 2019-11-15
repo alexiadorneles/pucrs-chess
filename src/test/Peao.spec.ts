c3a9a157c91d20985ce7431df9e0a0b66220cb97
alexiadorneles <lecavsalexia@gmail.com>
2019-11-15 17:56:25 -0300

    test(pawn): remove duplicated test case.

--- src/test/Peao.spec.ts
+++ src/test/Peao.spec.ts
@@ -3,6 +3,7 @@ import 'mocha'
 import { Cor } from '../main/definitions/Cor'
 import { ItemTabuleiro } from '../main/domain/ItemTabuleiro'
 import { Peao } from '../main/domain/peca/Peao'
+import { Tabuleiro } from '../main/domain/Tabuleiro'
 
 context('Peao', () => {
   describe('ao chamar adicionarAoItem', () => {
@@ -19,20 +20,11 @@ context('Peao', () => {
   describe('ao chamar simularMovimento', () => {
     it('quando caminho livre deve retornar posição atual e uma para frente', () => {
       // arrange
+      const tabuleiro = new Tabuleiro()
       const peao = new Peao(Cor.BRANCAS)
       const item = new ItemTabuleiro({ linha: 1, coluna: 2 }, Cor.PRETAS)
       item.atribuirPeca(peao)
-      // act
-      const esperado = [{ linha: 2, coluna: 2 }]
-      const resultado = peao.simularMovimento()
-      // assert
-      expect(resultado).to.deep.equals(esperado)
-    })
-    it('quando caminho livre deve retornar posição atual e uma para frente', () => {
-      // arrange
-      const peao = new Peao(Cor.BRANCAS)
-      const item = new ItemTabuleiro({ linha: 1, coluna: 2 }, Cor.PRETAS)
-      item.atribuirPeca(peao)
+      tabuleiro.adicionarItem(item)
       // act
       const esperado = [{ linha: 2, coluna: 2 }]
       const resultado = peao.simularMovimento()
